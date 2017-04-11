ab4bf7f361e58da8a317ba5f8a664c345c2b1103
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-04-11 15:51:24 -0400

    initial

---
+++ src/subset-shot-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const subsetShot = require('.')
+
+describe('subset-shot', () => {
+  it('write this test', () => {
+    console.assert(subsetShot, 'should export something')
+  })
+})
